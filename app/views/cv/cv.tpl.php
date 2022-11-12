ede7928f3d2e410402bbbdb2b17a4d7f4debd9b0
Mahieux-Nicolas <deadhunterx59@gmail.com>
2022-11-12 15:05:15 +0100

    mise en forme CV ok

--- app/views/cv/cv.tpl.php
+++ app/views/cv/cv.tpl.php
@@ -1,144 +1,158 @@
 <section class="cvhead">
-<body>
+
+    <body>
 
         <main>
-        <div class="left-part" >
-            <section class="intro">
-            <h2 class="intro">Développeur Web et Web mobile </h2>
-            <p class="intro">Je souhaite créer et développer des applications, des sites web ou résoudre
-                des anomalies d’outils.</p>
+            <div class="left-part">
+                <section class="intro">
+                    <h2 class="intro">Développeur Web </h2>
+                    <br>
+                    <p class="intro"><i>Je souhaite développer et maintenir des applications dans les langages de programmations que j’affectionne notamment sur le Framework Symfony.</i></p>
 
-            </section>
-            
-              
-                   
-            <section class="atouts">
-                <h3>Atouts</h3>
-                <h4>- Amélioration continue</h4>
+                </section>
+                <br>
+                <section class="atouts">
+                    <h3>Atouts</h3><br>
+                    <h4>Amélioration continue :</h4>
 
-                <ul>
-                    <li>
-                        - Création de divers programmes en VBA sur Excel pour améliorer
-                        l’efficacité opérationnelle de l’équipe.</li>
-                    <li>- Force de proposition sur l’amélioration des procédures, des outils.</li>
-                </ul>
-                <h4>Gestion de projets</h4>
-                <ul>
-                    <li> - Gestion d’un projet pour la gestion des dossiers d’indus d’assurés
-                        (Travail avec l’informatique pour le développement de l’outil, création
-                        d’une lettre type pour l’envoi des courriers de mise en demeure, création
-                        du mode opératoire, formation des collaborateurs et suivi),</li>
+                    <p>- Force de proposition sur l’amélioration des procédures, des outils, des applications.</p>
 
-                    <li>- Gestion d’un projet pour la création d’une procédure sur les dossiers
-                        d’indus clients (extraction des clients en anomalie, création d’un
-                        publipostage automatique, création du mode opératoire, organisation de
-                        points mensuels avec la comptabilité). Récupération d’environ 50k€ à la
-                        fin de ma mission
-                    </li>
-                    <li>- Pilotage d’activité
-                        - Pilotage d’une activité de gestion, avec animation des briefs d’équipe
-                        - Création d’indicateurs de performance, et d’outils pour piloter
-                        efficacement l’activité
-                        -Formation et accompagnement des collaborateurs sur les procédures et
-                        les outils</li>
+                    <br>
+                    <h4>Gestion de projets :</h4>
 
-                </ul>
+                    <p>- Création d’un projet de boutique en ligne en méthodologie AGILE (SCRUM) dans le cadre de la formation. Daily et deux sprints organisés.J’ai également mené deux projets dans mes anciennes expériences (création d’indus client et automatisation d’envoi de lettres de relances via l’outil informatique).</p>
 
-            </section>
-            <section class="exp">
-                <h3>Expériences</h3>
-                <h4>2017 – Aujourd’hui : Chargé de gestion et de Relation Clients BPCE,
-                    Villeneuve d’Ascq</h4>
-                <p>
-                    Gestion administrative et médicale des adhésions prévoyance /
-                    Amélioration continue et participation aux projets interne</p>
 
-                <h4>2015 – 2017 : Gestionnaire Frais de santé– Société Française de
-                    Prévention, Marcq-en-Barœul</h4>
-                <p>
-                    Saisie des remboursements mutuelle/Amélioration continue et gestion de
-                    projet
-                </p>
-                <h4>2013 – 2014 : Gestionnaire frais de santé– La mutuelle des étudiants,
-                    Marcq-en-Barœul</h4>
-                <p>
-                    Saisie des remboursements mutuelle/Amélioration continue et gestion de
-                    projet
-                </p>
 
-                <h4>2010 – 2011 : Gestionnaire – Crédit Agricole Nord de France, Lille</h4>
-                <p>
-                    Contrôle de complétude et de conformité des contrats titres et PEA/ Saisie
-                    et pilotage de données (reporting mensuel, analyse)
-                </p>
-            </section>
-       
-        
-            <section class="formation">
-                <h3>Formation</h3>
-                <h4>2022-2023 Ecole O'clock</h4>
-                <p>2009 : BTS Assistant de Gestion PME/PMI</p>
-                <h4>2009 : BTS Assistant de Gestion PME/PMI</h4>
-                <p>Lycée Sévigné, Tourcoing </p>
-                <h4>2007 : Baccalauréat Sciences des Technologies de la Gestion
-                    option Ressources humaines </h4>
-                <p>Lycée Sévigné, Tourcoing</p>
-            </section>
-        </div>
+                </section>
+                <br>
+                <section class="exp">
+                    <h3>Expériences</h3><br>
+                    <h4>2017 – Aujourd’hui : Chargé de gestion et de Relation Clients BPCE,
+                        Villeneuve d’Ascq</h4>
+                    <p>
+                        Gestion administrative et médicale des adhésions prévoyance / Amélioration continue et participation aux projets internes
+                        <b> Comptétences développées : </b> Analyse, relationnel, synthèse, écoute, informatique.
+                    </p>
+                    <br>
+                    <h4>2015 – 2017 : Gestionnaire Frais de santé– Société Française de
+                        Prévention, Marcq-en-Barœul</h4>
+                    <p>
+                        Saisie des remboursements mutuelle/Amélioration continue et gestion de projet.
+                    </p>
+                    <p> <b> Comptétences développées : </b> Relation client, empathie, autonomie</p>
+                    <br>
+                    <h4>2013 – 2014 : Gestionnaire frais de santé– La mutuelle des étudiants,
+                        Marcq-en-Barœul</h4>
+                    <p>
+                        Saisie des remboursements mutuelle/Amélioration continue et gestion de projet.
+                    </p>
+                    <p> <b> Comptétences développées : </b> respect des délais, gestion du stress.</b></p>
+                    <br>
+                    <h4>2010 – 2011 : Gestionnaire – Crédit Agricole Nord de France, Lille</h4>
+                    <p>
+                        Contrôle de complétude et de conformité des contrats titres et PEA/ Saisie et pilotage de données (reporting mensuel, analyse)
+                    <p><b> Comptétences développées : </b> organisation, analyse.</p>
+
+                    </p>
+                </section>
+                <br>
+
+
+                <section class="formation">
+                    <h3>Formations</h3><br>
+                    <h4>2022-2023 Développeur web & web mobile - 6 mois - temps plein</h4>
+                    <p>Ecole O'clock - téléprésentiel</p><br>
+                    <h4>2021- Réalisation d'un bilan de compétences</h4>
+                    <p>Parcours Chance</p><br>
+                    <h4>2009 : BTS Assistant de Gestion PME/PMI</h4>
+                    <p>Lycée Sévigné, Tourcoing </p><br>
+                    <h4>2007 : Baccalauréat Sciences des Technologies de la Gestion
+                        option Ressources humaines </h4>
+                    <p>Lycée Sévigné, Tourcoing</p>
+                </section>
+            </div>
             <div class="right-part">
-            <figure> <img class="phpto" src="<?=$viewData['baseUri']?>/../public/assets/images/moi.PNG" alt=""> </figure>
-            <address></address>
-            <section>
-                <h5>Qualités</h5>
-                <ul>
-                    <li>Curiosité</li>
-                    <li>Débrouillardise</li>
-                    <li>Adaptabilité</li>
-                    <li>Force de proposition</li>
-                </ul>
-            </section>
-            <section>
-                <h5>Valeurs</h5>
-                <ul>
-                    <li>Résilience</li>
-                    <li>Franchise</li>
-                    <li>Bienveillance</li>
+                <figure> <img class="phpto" src="<?= $viewData['baseUri'] ?>/../public/assets/images/moi.PNG" alt=""> </figure>
+                <div class="right-part__text">
+                    <section>
 
-                </ul>
-            </section>
-            <section>
-                <h5>Langues étrangères</h5>
-                <ul>
-                    <li>Anglais</li>
-                </ul>
-            </section>
-            <section>
-                <h5>Logiciels et langages de programmation</h5>
-                <ul>
-                    <li>HTML/CSS</li>
-                    <li>PHP</li>
-                    <li>Javascript</li>
-                    <li>React</li>
-                    <li>Python</li>
-                </ul>
-            </section>
-            <section>
-                <h5>autres activités</h5>
-                <ul>
-                    <li>Pratique du football en
-                        club</li>
-                    <li>Guitariste amateur</li>
-                    <li>Informatique</li>
-                    <li>jeux vidéos</li>
+                        <ul>
+                            <li class="skills">07.84.69.84.59</li>
+                            <li class="skills">mahieux.nicolas1@laposte.net</li>
+                            <a href="http://www.linkedin.com/in/nicolasmhx-developpeur-back-end" style="color:white; text-decoration:underline">Linkedin</a></li>
 
-                </ul>
-            </section>
-        </div>
-        <a class="btnCv" href="<?=$viewData['baseUri']?>/../public/assets/images/moi.PNG" download>télécharger le CV</a>
-    </main>
+                        </ul>
+                    </section>
+                    <br>
+                    <hr>
+                    <br>
+                    <section>
+                        <h4>Qualités</h4> <br>
+                        <ul>
+                            <li class="skills">Curiosité</li>
+                            <li class="skills">Débrouillardise</li>
+                            <li class="skills">Adaptabilité</li>
+                            <li class="skills">Force de proposition</li>
+                        </ul>
+                    </section>
+                    <br>
+                    <hr>
+                    <br>
+                    <section>
+                        <h4>Valeurs</h4><br>
+                        <ul>
+                            <li class="skills">Résilience</li>
+                            <li class="skills">Franchise</li>
+                            <li class="skills">Bienveillance</li>
+
+                        </ul>
+                    </section>
+                    <br>
+                    <hr>
+                    <br>
+                    <section>
+                        <h4>Langues étrangères</h4><br>
+                        <ul>
+                            <li class="skills">Anglais</li>
+                        </ul>
+                    </section>
+                    <br>
+                    <hr>
+                    <br>
+                    <section>
+                        <h4>Compétences techniques</h4><br>
+                        <ul>
+                            <li class="skills">HTML/CSS, SEO</li>
+                            <li class="skills">Responsive Web Design</li>
+                            <li class="skills">PHP, Javascript</li>
+                            <li class="skills">MySQL</li>
+                            <li class="skills">Bootstrap</li>
+                            <li class="skills">Frameworks : Laravel, Symfony</li>
+                            <li class="skills">Gestion de projet en méthodologie AGILE (SCRUM)</li>
+                            <li class="skills">GitHub</li>
+                        </ul>
+                    </section>
+                    <br>
+                    <hr>
+                    <br>
+                    <section>
+                        <h4>Autres activités</h4><br>
+                        <ul>
+                            <li class="skills">Pratique du football en
+                                club depuis 15 ans</li>
+                            <li class="skills">Guitariste amateur</li>
 
 
-</body>
+                        </ul>
+                    </section>
+                </div>
+            </div>
+            <a class="btnCv" href="<?= $viewData['baseUri'] ?>/../public/assets/images/moi.PNG" download>télécharger le CV</a>
+        </main>
+
+
+    </body>
 
 
 
